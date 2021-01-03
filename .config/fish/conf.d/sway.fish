3a6718dafd7372fb5bfccf87971a9255ae389f43
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-01-03 15:42:49 +0100

    sway: load systemd integration via distro package config

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,6 +1,9 @@
+# Make systemd aware of modified PATH
+systemctl --user import-environment PATH
+
 # Running from tty1 start sway
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
 	systemd-cat -t sway sway
+	systemctl --user stop sway-session.target
 end
-
