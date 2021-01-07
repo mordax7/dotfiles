f016c5eb7c2fa5050fe66eaa6fb37dee3d8d929e
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-01-07 15:48:06 +0100

    ansible: add yubikey authentication method

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -6,6 +6,10 @@ for line in (systemctl --user show-environment)
     set -x "$key" "$value"
 end
 
+set GPG_TTY "$TTY"
+set SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+systemctl --user import-environment GPG_TTY SSH_AUTH_SOCK
+
 # Running from tty1 start sway
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
