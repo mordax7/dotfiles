035aac237031bb0f73159a200bcb8fdbac7c051d
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-11-05 20:11:00 +0100

    fish: add missing key bindings for todoist

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,11 @@
 function fish_user_key_bindings
+	# fzf
 	fzf_key_bindings
+
+    # todoist
+    bind -M insert \ctt fzf_todoist_item
+    bind -M insert \ctp fzf_todoist_project
+    bind -M insert \ctl fzf_todoist_labels
+    bind -M insert \ctc fzf_todoist_close
+    bind -M insert \ctd fzf_todoist_delete
 end
