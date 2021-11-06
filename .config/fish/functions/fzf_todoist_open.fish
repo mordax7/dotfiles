91854762e7aaee126728408b86013ab1aacdd75c
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-11-06 13:09:40 +0100

    fisher: upstream update fzf todoist cli function

--- .config/fish/functions/fzf_todoist_open.fish
+++ .config/fish/functions/fzf_todoist_open.fish
@@ -1,5 +1,5 @@
 function fzf_todoist_open
-  todoist list | fzf | cut -d ' ' -f 1 | tr '\n' ' ' | sed -e 's/[ \t]*$//' | read ret
+  todoist list | fzf -q (LC_TIME=en_US.utf8 date '+%y/%m/%d(%a)') | cut -d ' ' -f 1 | tr '\n' ' ' | sed -e 's/[ \t]*$//' | read ret
   if [ $ret ]
     todoist show --browse $ret
     commandline -f repaint
