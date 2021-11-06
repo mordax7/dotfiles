6905a12a7b905676224ede0d69762219fe1af3a9
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-11-06 12:49:34 +0100

    fish: switch to my own todoist fzf functions

--- .config/fish/functions/fzf_todoist_close.fish
+++ .config/fish/functions/fzf_todoist_close.fish
@@ -1,5 +1,5 @@
 function fzf_todoist_close
-  todoist list | fzf | cut -d ' ' -f 1 | tr '\n' ' ' | sed -e 's/[ \t]*$//' | read ret 
+  todoist list | fzf -q (LC_TIME=en_US.utf8 date '+%y/%m/%d(%a)') | cut -d ' ' -f 1 | tr '\n' ' ' | sed -e 's/[ \t]*$//' | read ret
   if [ $ret ]
     todoist close $ret
     commandline -f repaint
