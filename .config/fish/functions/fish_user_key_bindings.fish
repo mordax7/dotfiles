1de72cada93dd194579a5f23a1edf8b3ef6238a8
Aljaz Gantar <gantar.aljaz94@gmail.com>
2021-11-07 12:00:28 +0100

    fish: change todoist key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,11 +3,11 @@ function fish_user_key_bindings
     fzf_key_bindings
 
     # todoist
-    bind -M insert \cti fzf_todoist_item
-    bind -M insert \ctp fzf_todoist_project
-    bind -M insert \ctl fzf_todoist_labels
-    bind -M insert \ctc fzf_todoist_close
-    bind -M insert \ctd fzf_todoist_delete
-    bind -M insert \cto fzf_todoist_open
-    bind -M insert \ctt fzf_todoist_date
+    bind -M insert \eti fzf_todoist_item
+    bind -M insert \etp fzf_todoist_project
+    bind -M insert \etl fzf_todoist_labels
+    bind -M insert \etc fzf_todoist_close
+    bind -M insert \etd fzf_todoist_delete
+    bind -M insert \eto fzf_todoist_open
+    bind -M insert \ett fzf_todoist_date
 end
